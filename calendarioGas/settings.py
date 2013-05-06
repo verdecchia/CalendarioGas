e6f909f821a976642c54b081e8d789df459d82b3
m.verdecchia <marco.verdecchia@gmail.com>
2013-05-06 22:09:07 +0200

    add static path url

--- calendarioGas/settings.py
+++ calendarioGas/settings.py
@@ -1,5 +1,8 @@
 # Django settings for calendarioGas project.
 
+import os.path
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -72,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_PATH + "/static/",
 )
 
 # List of finder classes that know how to find static files in
